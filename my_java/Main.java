1cfc1ff8bd8e87203a7af9d3c878bafaf12aca92
fd <1397818213@qq.com>
2023-03-09 17:50:45 +0800

    ok hello world

--- my_java/Main.java
+++ my_java/Main.java
@@ -1,10 +1,10 @@
 package my_java;
 
-import my_java.encryption.*;
-
 public class Main {
 
     public static void main(String[] args) {
-        new Frame();
+
+        System.out.println("hello world");
+
     }
 }
--- my_python/main.py
+++ my_python/main.py
@@ -1,8 +1,6 @@
 
-import data_structure.Tree as T
-
 if __name__ == '__main__':
     
-    T.test()
+    print('hello world')
 
     pass
